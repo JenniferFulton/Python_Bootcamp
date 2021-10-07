605392f73db0c97c99032796004a59b55f2f3c25
JenniferFulton <jennifer.reeves8@gmail.com>
2021-10-07 18:01:09 -0400

    Algorithm for #6 complete. Else statement with pass added to #3

--- for_loop_basicI.py
+++ for_loop_basicI.py
@@ -12,6 +12,8 @@ for i in range(101):
         print("Coding Dojo")
     elif i % 5 == 0:
         print("Coding")
+    else:
+        pass
 
 #4 Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 sum = 0
@@ -21,6 +23,16 @@ print(sum)
 
 
 #5 Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+for positive_num in range (2018,0,-4):
+    print(positive_num)
 
 
 #6 Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+low_num = 2
+high_num = 33
+mult = 8
+for number in range(low_num, high_num, 1):
+    if number % mult == 0:
+        print(number)
+    else:
+        pass
