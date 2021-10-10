c3f2cf80415ad62afd6808d46327db3845f1a570
JenniferFulton <jennifer.reeves8@gmail.com>
2021-10-10 10:49:03 -0400

    print and return function created and working

--- functions_basicII.py
+++ functions_basicII.py
@@ -8,10 +8,13 @@ def countdown(num):
     return countdown_list
 print(countdown(10))
 
-
-
 # Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 # Example: print_and_return([1,2]) should print 1 and return 2
+
+def print_and_return(list_of_two):
+        print(list_of_two[0])
+        return list_of_two[1]
+print(print_and_return([5,23]))
 
 # First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
 # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
