b9e89e423cd2298343435f9fb191d8a429761b0f
JenniferFulton <jennifer.reeves8@gmail.com>
2021-10-17 17:31:08 -0400

    Able to update all values in dictionaries and lists in prompt #1

--- functions_intermediateII.py
+++ functions_intermediateII.py
@@ -11,12 +11,17 @@ sports_directory = {
 z = [ {'x': 10, 'y': 20} ]
 
 # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
-
+x[1][0] = 15
+print(x)
 # Change the last_name of the first student from 'Jordan' to 'Bryant'
-
+students[0]['last_name'] = 'Bryant'
+print(students)
 # In the sports_directory, change 'Messi' to 'Andres'
-
+sports_directory['soccer'][0] = "Andres"
+print(sports_directory)
 # Change the value 20 in z to 30
+z[0]['y'] = 30
+print(z)
 
 #2. Iterate Through a List of Dictionaries
 #Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
@@ -26,7 +31,8 @@ students = [
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-iterateDictionary(students) 
+def iterate_dictionary(dictionary):
+iterate_dictionary(students) 
 
 
 #3. Get Values From a List of Dictionaries
@@ -39,5 +45,5 @@ dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-printInfo(dojo)
+# printInfo(dojo)
 
