67c9a5f0768b0bcb0857e5261e38418cec66289f
JenniferFulton <jennifer.reeves8@gmail.com>
2021-10-20 16:11:53 -0400

    withdrawal method created, balance display method created, and 3 users added

--- user.py
+++ user.py
@@ -1,5 +1,9 @@
-# 
+# For this assignment, we'll add some functionality to the User class:
 
+# make_withdrawal(self, amount) - have this method decrease the user's balance by the amount specified
+# display_user_balance(self) - have this method print the user's name and account balance to the terminal
+# eg. "User: Guido van Rossum, Balance: $150
+# BONUS: transfer_money(self, other_user, amount) - have this method decrease the user's balance by the amount and add that amount to other other_user's balance
 class User:
     def __init__(self, name, bank):
         self.name = name
@@ -8,3 +12,15 @@ class User:
     
     def make_deposits(self, amount):
         self.balance += amount
+        return self
+    
+    def make_withdrawal(self,amount):
+        self.balance -= amount
+        return self
+    
+    def display_balance(self):
+        print(self.balance)
+
+Jen = User("Jennifer","PNC")
+Merg = User("Morgan", "BOA")
+Wendy = User("Wendy", "PNC")
