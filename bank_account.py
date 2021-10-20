2c9353cc45826cdf6987fda7742f4768d62e0650
JenniferFulton <jennifer.reeves8@gmail.com>
2021-10-20 19:34:58 -0400

    Bank Account class created with balance and interest rate attributes created

--- bank_account.py
+++ bank_account.py
@@ -6,3 +6,11 @@
 # withdraw(self, amount) - decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
 # display_account_info(self) - print to the console: eg. "Balance: $100"
 # yield_interest(self) - increases the account balance by the current balance * the interest rate (as long as the balance is positive)
+
+
+class BankAccount:
+    def __init__(self, balance, interest_rate):
+        self.balance = balance
+        self.interest_rate = interest_rate
+    
+    
