72902ed235f09e58a6128538590d5adce74abca5
JenniferFulton <jennifer.reeves8@gmail.com>
2021-10-24 21:03:32 -0400

    sleep,eat and play method added to Pet class

--- dojo_pets.py
+++ dojo_pets.py
@@ -25,3 +25,18 @@ class Pet:
         self.name = name
         self.type = type
         self.tricks = tricks
+        self.energy = 0
+        self.health = 0
+    
+    def sleep(self):
+        self.energy += 25
+        return self
+    
+    def eat(self):
+        self.energy += 5
+        self.health += 10
+        return self
+    
+    def play(self):
+        self.health += 5
+        return self
