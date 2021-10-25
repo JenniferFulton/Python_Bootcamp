ae350a1c8372c20a240804d3a7fc5b1e281f959f
JenniferFulton <jennifer.reeves8@gmail.com>
2021-10-24 21:09:46 -0400

    noise method added to Pet class. Pet class placed infront of Ninja class so that I can invoke methods from the Pet class in the ninja class

--- dojo_pets.py
+++ dojo_pets.py
@@ -1,18 +1,3 @@
-# implement __init__( first_name , last_name , treats , pet_food , pet )
-# implement the following methods:
-# walk() - walks the ninja's pet invoking the pet play() method
-# feed() - feeds the ninja's pet invoking the pet eat() method
-# bathe() - cleans the ninja's pet invoking the pet noise() method
-
-class Ninja:
-    def __init__(self, first_name , last_name , treats , pet_food , pet):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.treats = treats
-        self.pet_food = pet_food
-        self.pet = pet
-        
-
 # implement __init__( name , type , tricks ):
 # implement the following methods:
 # sleep() - increases the pets energy by 25
@@ -40,3 +25,22 @@ class Pet:
     def play(self):
         self.health += 5
         return self
+
+    def noise(self,noise):
+        print(noise)
+        return self
+
+# implement __init__( first_name , last_name , treats , pet_food , pet )
+# implement the following methods:
+# walk() - walks the ninja's pet invoking the pet play() method
+# feed() - feeds the ninja's pet invoking the pet eat() method
+# bathe() - cleans the ninja's pet invoking the pet noise() method
+
+class Ninja:
+    def __init__(self, first_name , last_name , treats , pet_food , pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
+        
